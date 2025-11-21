06734ce52cd6ea1d74a6bf211be68c5f420b8610
Travis Boltz <tboltz07@gmail.com>
2025-11-21 15:49:22 -0500

    added bat and file

---
+++ run_scan.bat
@@ -0,0 +1,4 @@
+@echo off
+cd C:\Users\suzan\Projects
+C:\Users\suzan\AppData\Local\Programs\Python\Python312\python.exe -m retailer_selector.orchestrator --limit 10 --concurrency 5
+echo This runs the scan
